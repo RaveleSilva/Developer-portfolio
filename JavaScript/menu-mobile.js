6a39eef9d3f7ae27ad806a1b74255477d8291d9e
Ravele Silva <ravele.cs@gmail.com>
2023-09-18 22:10:21 -0300

    Ajustes menu mobile

--- JavaScript/menu-mobile.js
+++ JavaScript/menu-mobile.js
@@ -1,6 +1,18 @@
 export default function menuMobile() {
   const menuButton = document.querySelector('.menu-mobile');
   const menuNav = document.querySelector('.header nav');
+  const header = document.querySelector('.header');
+  const socialHeader = document.querySelector('.social-header');
+
+  function inserirSocialHeader() {
+    if (window.innerWidth < 750) {
+      menuNav.appendChild(socialHeader);
+    } else if (menuNav.contains(socialHeader)) {
+      menuNav.removeChild(socialHeader);
+      header.appendChild(socialHeader);
+      header.insertBefore(menuNav, socialHeader);
+    }
+  }
 
   function openMenu() {
     menuNav.classList.toggle('active');
@@ -8,5 +20,6 @@ export default function menuMobile() {
   }
 
   menuButton.addEventListener('click', openMenu);
+  window.addEventListener('load', inserirSocialHeader);
+  window.addEventListener('resize', inserirSocialHeader);
 }
-
