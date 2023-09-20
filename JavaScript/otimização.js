3301b3e4fb625d45c00cd668a1f7afb08d72e3a0
Ravele Silva <ravele.cs@gmail.com>
2023-09-20 17:49:08 -0300

    Otimização de imagens

---
+++ JavaScript/otimização.js
@@ -0,0 +1,4 @@
+export default function otimizar() {
+  const introBg = new Image();
+  introBg.src = "../img/intro-img.png";
+}
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,2 @@
 import menuMobile from "./menu-mobile.js";
-
 menuMobile();
