7a15511f42479cf7620a324b60436ff095475d7d
Ravele Silva <ravele.cs@gmail.com>
2023-09-22 20:08:47 -0300

    Efeito de autotype implementado

---
+++ JavaScript/autotype.js
@@ -0,0 +1,15 @@
+export default function autoType(element) {
+  const elemento = document.querySelector(element)
+  const tituloFrase = elemento.innerText
+  elemento.innerText = ''
+  let count = 0;
+  const myInterval = setInterval(() => {
+    
+    elemento.innerHTML += tituloFrase.charAt(count)
+    count++
+
+    if (count > tituloFrase.length){
+      clearInterval(myInterval)
+    }
+  }, 75)
+}
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,5 @@
 import menuMobile from "./menu-mobile.js";
+import autoType from "./autotype.js";
 menuMobile();
+autoType('.home-text h1')
+
