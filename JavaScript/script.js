0f749ca800f4648b982c8ac61da5ef9daa03bb8a
Ravele Silva <ravele.cs@gmail.com>
2023-10-30 21:48:04 -0300

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,10 +5,3 @@ import animaScroll from "./anima-scroll.js";
 menuMobile();
 autoType('.home-text h1');
 animaScroll();
-
-const submit = document.querySelector('form button')
-
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.alert('O GitHub Pages não tem suporte para servidores Node. Em breve estarei hospedando o site em um domínio próprio :)')
-})
